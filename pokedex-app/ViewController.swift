e0d4c23c83c77f97a642eb9aa48ae0e14d536a60
Andrew Nicholson <anicholson@clicksco.com>
2015-11-23 19:09:27 +0000

    Initial Commit

---
+++ pokedex-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-app
+//
+//  Created by Andrew Nicholson on 23/11/2015.
+//  Copyright © 2015 Nicholson Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
