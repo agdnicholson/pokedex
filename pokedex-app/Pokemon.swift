6371541dc04010b8821ff127aaff52c4a2db2a2c
Andrew Nicholson <anicholson@clicksco.com>
2015-11-26 20:40:25 +0000

    App complete. Part 14 included plus some tidy up.

--- pokedex-app/Pokemon.swift
+++ pokedex-app/Pokemon.swift
@@ -19,6 +19,8 @@ class Pokemon {
     private var _weight: String!
     private var _attack: String!
     private var _nextEvolutionTxt: String!
+    private var _nextEvolutionId: String!
+    private var _nextEvolutionLevel: String!
     private var _pokemonUrl: String!
     
     var name: String {
@@ -27,6 +29,69 @@ class Pokemon {
     
     var pokedexId: Int {
         return _pokedexId
+    }
+    
+    var description: String {
+        if _description == nil {
+            _description = ""
+        }
+        return _description
+    }
+    
+    var type: String {
+        if _type == nil {
+            _type = ""
+        }
+        return _type
+    }
+    
+    var defense: String {
+        if _defense == nil {
+            _defense = ""
+        }
+        return _defense
+    }
+    
+    var height: String {
+        if _height == nil {
+            _height = ""
+        }
+        return _height
+    }
+    
+    var weight: String {
+        if _weight == nil {
+            _weight = ""
+        }
+        return _weight
+    }
+    
+    var attack: String {
+        if _attack == nil {
+            _attack = ""
+        }
+        return _attack
+    }
+    
+    var nextEvolutionTxt: String {
+        if _nextEvolutionTxt == nil {
+            _nextEvolutionTxt = ""
+        }
+        return _nextEvolutionTxt
+    }
+    
+    var nextEvolutionId: String {
+        if _nextEvolutionId == nil {
+            _nextEvolutionId = ""
+        }
+        return _nextEvolutionId
+    }
+    
+    var nextEvolutionLvl: String {
+        if _nextEvolutionLevel == nil {
+            _nextEvolutionLevel = ""
+        }
+        return _nextEvolutionLevel
     }
     
     init(name: String, pokedexId: Int){
@@ -80,6 +145,50 @@ class Pokemon {
                 }
                 
                 print(self._type)
+                
+                //Description
+                if let descArr = dict["descriptions"] as? [Dictionary<String, String>] where descArr.count>0 {
+                    if let url = descArr[0]["resource_uri"] {
+                        let nsurl = NSURL(string: "\(URL_BASE)\(url)")!
+                        Alamofire.request(.GET, nsurl).responseJSON { response in
+                            let descResult = response.result
+                            
+                            if let descDict = descResult.value as? Dictionary<String, AnyObject> {
+                                if let description = descDict["description"] as? String {
+                                    self._description = description
+                                    print(self._description)
+                                }
+                            }
+                            completed()
+                        }
+                    }
+                } else {
+                    self._description = ""
+                }
+                
+                //Evolutions
+                if let evolutions = dict["evolutions"] as? [Dictionary<String, AnyObject>] where evolutions.count > 0{
+                    if let to = evolutions[0]["to"] as? String {
+                        
+                        //Can't support mega pokemon right now but api still has mega data
+                        if to.rangeOfString("mega") == nil {
+                            if let uri = evolutions[0]["resource_uri"] as? String {
+                                let newStr = uri.stringByReplacingOccurrencesOfString("/api/v1/pokemon/", withString: "")
+                                let num = newStr.stringByReplacingOccurrencesOfString("/", withString: "")
+                                self._nextEvolutionId = num
+                                self._nextEvolutionTxt = to
+                                if let lvl = evolutions[0]["level"] as? Int {
+                                    self._nextEvolutionLevel = "\(lvl)"
+                                }else {
+                                    self._nextEvolutionLevel = ""
+                                }
+                                print(self._nextEvolutionId)
+                                print(self._nextEvolutionTxt)
+                                print(self._nextEvolutionLevel)
+                            }
+                        }
+                    }
+                }
             }
         }
     }
--- pokedex-app/PokemonDetailVC.swift
+++ pokedex-app/PokemonDetailVC.swift
@@ -24,18 +24,71 @@ class PokemonDetailVC: UIViewController {
     @IBOutlet weak var currentEvoImg: UIImageView!
     @IBOutlet weak var nextEvoImg: UIImageView!
     @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var evoStrip: UIView!
+    @IBOutlet weak var bioStackView: UIStackView!
+    @IBOutlet weak var evoStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        nameLbl.text = pokemon.name
-        mainImg.image = UIImage(named: "\(pokemon.pokedexId)")
+        nameLbl.text = pokemon.name.capitalizedString
+        let img = UIImage(named: "\(pokemon.pokedexId)")
+        mainImg.image = img
+        currentEvoImg.image = img
+        
+        //just hide the current evolution as it was suppossed to be previous. No point showing this.
+        currentEvoImg.hidden = true
+        
+        //clear labels because it will take a second to download all this stuff.
+        clearUI()
         
         pokemon.downloadPokemonDetails { () -> () in
             //this will be called after download is done
+            self.updateUI()
         }
     }
-
+    
+    func clearUI() {
+        descriptionLbl.text = ""
+        typeLbl.text = ""
+        defenseLbl.text = ""
+        pokedexIdLbl.text = ""
+        heightLbl.text = ""
+        weightLbl.text = ""
+        baseAttackLbl.text = ""
+        evoLbl.text = ""
+        nextEvoImg.hidden = true
+        evoStrip.hidden = true
+        bioStackView.hidden = true
+        evoStackView.hidden = true
+    }
+    
+    func updateUI() {
+        descriptionLbl.text = pokemon.description
+        typeLbl.text = pokemon.type
+        defenseLbl.text = pokemon.defense
+        pokedexIdLbl.text = "\(pokemon.pokedexId)"
+        heightLbl.text = pokemon.height
+        weightLbl.text = pokemon.weight
+        baseAttackLbl.text = pokemon.attack
+        
+        if pokemon.nextEvolutionId == "" {
+            evoLbl.text = "No Evolutions"
+            nextEvoImg.hidden = true
+        }else {
+            nextEvoImg.hidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            var evoStr = "Next Evolution: \(pokemon.nextEvolutionTxt)"
+            if pokemon.nextEvolutionLvl != "" {
+                evoStr += " - LVL \(pokemon.nextEvolutionLvl)"
+            }
+            evoLbl.text = evoStr
+        }
+        evoStrip.hidden = false
+        bioStackView.hidden = false
+        evoStackView.hidden = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
